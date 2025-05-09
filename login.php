5490948500cc38721df51769f5ff8164582b89c2
Deepasree-MP <dmeenapadmanabhan@algomau.ca>
2025-05-09 21:35:12 +0000

    Added validate page with if statements
    added hardcoded username and password

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
       <input type="text" id="username" name="username" >
         <br>
       <label for="password">Password:</label><br>
-      <input type="password" id="password" name="lname" >
+      <input type="password" id="password" name="password" >
         <br>
         <br>
       <input type="submit" value="Submit">
